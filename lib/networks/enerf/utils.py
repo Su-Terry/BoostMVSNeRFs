4421f1c5aa611ce8b2bb37232100dad9376842bc
Su-Terry <xx60507920530@gmail.com>
2024-07-20 20:03:23 +0800

    update config

--- lib/networks/enerf/utils.py
+++ lib/networks/enerf/utils.py
@@ -644,17 +644,13 @@ def raw2outputs_blend(raws, masks, z_vals, white_bkgd=False):
     alphas = torch.sum(alpha_all * masks, dim=1) # [B, N_rays, N_samples]
     
     rgb_all = raws[..., :3]
-    # T_all = torch.cumprod(torch.cat([torch.ones_like(alpha_all[..., 0:1]), 1-alpha_all], dim=-1), dim=-1)[..., :-1]
     T_full = torch.cumprod(torch.cat([torch.ones_like(alphas[..., 0:1]), 1-alphas], dim=-1), dim=-1)[..., :-1] # [B, N_rays, N_samples]
     
-    # weights_all = alpha_all * T_all # [B, K, N_rays, N_samples]
     weights_full = alphas * T_full # [B, N_rays, N_samples]
     
-    # rgb_map_all = torch.sum(weights_all[...,None] * rgb_all, -2)  # [B, K, N_rays, 3]
     rgb_map_full = torch.sum((T_full.unsqueeze(1).repeat(1, K, 1, 1)*alpha_all*masks)[...,None] * rgb_all, -2)  # [B, K, N_rays, 3]
     rgb_map_full = torch.sum(rgb_map_full, dim=1) # [B, N_rays, 3]
     
-    # depth_map_all = torch.sum(weights_all*z_vals.detach(), -1)
     depth_map_full = torch.sum(weights_full.unsqueeze(1).repeat(1, K, 1, 1)*z_vals.detach(), -1)
     
     if white_bkgd:
--- lib/visualizers/enerf.py
+++ lib/visualizers/enerf.py
@@ -1,14 +1,9 @@
-import matplotlib.pyplot as plt
-from lib.utils import data_utils
-from lib.utils import img_utils
 from lib.config import cfg
 import numpy as np
-import torch.nn.functional as F
-import torch
 import imageio
 import os
 import cv2
-from skimage.exposure import rescale_intensity
+from PIL import Image
 
 class Visualizer:
     def __init__(self,):
@@ -23,44 +18,36 @@ class Visualizer:
         B, S, _, H, W = batch['src_inps'].shape
         i = cfg.enerf.cas_config.num - 1
         render_scale = cfg.enerf.cas_config.render_scale[i]
-        h, w = int(H*render_scale), int(W*render_scale)
+        h_render, w_render = int(H*render_scale), int(W*render_scale)
         assert(B == 1)
-        pred_rgb = output[f'rgb_level{i}'].reshape(h, w, 3).detach().cpu().numpy()
-        # depth = output[f'depth_level{i}'].reshape(h, w).detach().cpu().numpy()
-        # crop_h, crop_w = int(h * 0.1), int(w * 0.1)
-        # pred_rgb = pred_rgb[crop_h:, crop_w:-crop_w]
-        # depth = depth[crop_h:, crop_w:-crop_w]
+        pred_rgb = output[f'rgb_level{i}'].reshape(h_render, w_render, 3).detach().cpu().numpy()
+        pred_rgb = Image.fromarray((pred_rgb*255.).astype(np.uint8))
+        pred_rgb = pred_rgb.resize((640, 480), Image.LANCZOS)
+        pred_rgb = np.array(pred_rgb).astype(np.float32) / 255.
+        depth = output[f'depth_level{i}'].reshape(h_render, w_render).detach().cpu().numpy()
+        depth = Image.fromarray(depth)
+        depth = depth.resize((640, 480), Image.LANCZOS)
+        depth = np.array(depth)
+        
         self.imgs.append(pred_rgb)
-        # self.depths.append(depth)
+        self.depths.append(depth)
         if cfg.save_result:
             frame_id = batch['meta']['frame_id'][0].item()
             pred_rgb = (np.array(pred_rgb)*255).astype(np.uint8)
             imageio.imwrite(os.path.join(cfg.result_dir, 'imgs/{:06d}_rgb.jpg'.format(frame_id)), pred_rgb)
-            # imageio.imwrite(os.path.join(cfg.result_dir, 'imgs/{:06d}_dpt.jpg'.format(frame_id)), ((depth - depth.min()) / (depth.max() - depth.min()) * 255).astype(np.uint8))
+            imageio.imwrite(os.path.join(cfg.result_dir, 'imgs/{:06d}_dpt.jpg'.format(frame_id)), ((depth - depth.min()) / (depth.max() - depth.min()) * 255).astype(np.uint8))
 
     def summarize(self):
         img_rgb = (np.array(self.imgs)*255).astype(np.uint8)
         imageio.mimwrite(os.path.join(cfg.result_dir, 'color.mp4'), img_rgb, fps=60)
         
-        color1 = (0, 0, 255)     #red
-        color2 = (0, 165, 255)   #orange
-        color3 = (0, 255, 255)   #yellow
-        color4 = (255, 255, 0)   #cyan
-        color5 = (255, 0, 0)     #blue
-        color6 = (128, 64, 64)   #violet
-        colorArr = np.array([[color1, color2, color3, color4, color5, color6]], dtype=np.uint8)
-        # resize lut to 256 (or more) values
-        lut = cv2.resize(colorArr, (256,1), interpolation = cv2.INTER_LINEAR)
+        depths_rgb = []
+        for depth in self.depths:
+            depth = (depth - depth.min()) / (depth.max() - depth.min())
+            depth = cv2.applyColorMap((depth*255).astype(np.uint8), cv2.COLORMAP_JET)
+            depths_rgb.append(depth)
         
-        # Color depth map
-        # depths_gray = [ ((dpt - dpt.min()) / (dpt.max() - dpt.min()) * 255).astype(np.uint8) for dpt in self.depths ]
-        # depths_gray = [ rescale_intensity(dpt, in_range='image', out_range=(0, 255)).astype(np.uint8) for dpt in depths_gray ]
-        # depths_rgb = [ cv2.merge((dpt, dpt, dpt)) for dpt in depths_gray ]
-        
-        # apply lut
-        # depths_rgb = [ cv2.LUT(dpt, lut) for dpt in depths_rgb ]
-        
-        # imageio.mimwrite(os.path.join(cfg.result_dir, 'depth.mp4'), depths_rgb, fps=30)
+        imageio.mimwrite(os.path.join(cfg.result_dir, 'depth.mp4'), depths_rgb, fps=60)
         print('Save visualization results into {}'.format(cfg.result_dir))
 
 
